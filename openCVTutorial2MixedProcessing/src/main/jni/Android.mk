3cc0e0a07764127f26bd108e9cf353322377ea1a
fserena <fserena@student.ethz.ch>
2017-06-01 11:34:47 +0200

    Compiles

--- openCVTutorial2MixedProcessing/src/main/jni/Android.mk
+++ openCVTutorial2MixedProcessing/src/main/jni/Android.mk
@@ -2,7 +2,11 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-include ../../sdk/native/jni/OpenCV-tegra3.mk
+##include ../../sdk/native/jni/OpenCV-tegra3.mk
+
+OPENCVROOT:=/home/fs/NVPACK/OpenCV-2.4.8.2-Tegra-sdk
+## include ${OPENCVROOT}/sdk/native/jni/OpenCV.mk
+include ${OPENCVROOT}/sdk/native/jni/OpenCV-tegra3.mk
 
 LOCAL_MODULE    := mixed_sample
 LOCAL_SRC_FILES := jni_part.cpp
