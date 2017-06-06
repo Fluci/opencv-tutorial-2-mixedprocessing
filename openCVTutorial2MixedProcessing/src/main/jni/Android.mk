54516ae497a876c2da580b4f0e424266abe11c79
fserena <fserena@student.ethz.ch>
2017-06-06 10:52:11 +0200

    Clean up

--- openCVTutorial2MixedProcessing/src/main/jni/Android.mk
+++ openCVTutorial2MixedProcessing/src/main/jni/Android.mk
@@ -5,6 +5,7 @@ include $(CLEAR_VARS)
 ##include ../../sdk/native/jni/OpenCV-tegra3.mk
 
 OPENCVROOT:=/home/fs/NVPACK/OpenCV-2.4.8.2-Tegra-sdk
+#OPENCV_LIB_TYPE:=STATIC
 ## include ${OPENCVROOT}/sdk/native/jni/OpenCV.mk
 include ${OPENCVROOT}/sdk/native/jni/OpenCV-tegra3.mk
 
